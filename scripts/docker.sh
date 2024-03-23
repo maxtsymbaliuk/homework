a5f0218aa98f17f3fa8135a49ffe2f8d844039eb
max <maksym@mail.com>
2024-03-23 09:10:33 +0200

    add script docker

---
+++ scripts/docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+su - ${USER}
+sudo usermod -aG docker max
