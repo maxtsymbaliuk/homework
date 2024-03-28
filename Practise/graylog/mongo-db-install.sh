c5959007078f5988cedccb839cbfdb7ce850da23
max <max@gmail.com>
2024-03-28 08:21:22 +0000

    add mongodb script

---
+++ Practise/graylog/mongo-db-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-server-6.0.gpg 
+echo "deb [ arch=amd64,arm64 signed=/etc/apt/trusted.gpg.d/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
+sudo apt install mongodb-org -y
+mongod --version
+systemctl start mongod
+systemctl enable mongod
+systemctl status mongod
