1692e954a9797ea9aecc70cf54cde163bc1bbbbb
maxtsymbaliuk <153988575+maxtsymbaliuk@users.noreply.github.com>
2024-03-28 17:46:25 +0200

    Add files via upload

---
+++ Homework-Den/Step-project/install-jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'it`s vm2 script!!!'
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
