5a9ccd3cd2fba5de2ccdc41c81a36e91c8312f43
max <max@max.com>
2024-03-29 10:35:39 +0000

    add graylog files

--- Practise/graylog/sidecar.sh
+++ Practise/graylog/sidecar.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 wget https://packages.graylog2.org/repo/packages/graylog-sidecar-repository_1-5_all.deb
-sudo dpkg -i graylog-sidecar-repository_1-5_all.deb
-sudo apt-get update && sudo apt-get install graylog-sidecar            
+dpkg -i graylog-sidecar-repository_1-5_all.deb
+apt-get update && sudo apt-get install graylog-sidecar            
+graylog-sidecar -service install
+systemctl enable graylog-sidecar
+systemctl start graylog-sidecar
