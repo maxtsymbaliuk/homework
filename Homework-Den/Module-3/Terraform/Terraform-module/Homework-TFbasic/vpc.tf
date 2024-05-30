6037dbcebcc8acd31440e4205e321b7ea393cbb6
vagrant <vagrant@ubuntu>
2024-05-30 06:34:54 -0400

    add tf files to create ec2

--- Homework-Den/Module-3/Terraform/Terraform-module/Homework-TFbasic/vpc.tf
+++ Homework-Den/Module-3/Terraform/Terraform-module/Homework-TFbasic/vpc.tf
@@ -10,11 +10,12 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
 
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.${count.index*4}.0/22"
+  cidr_block = "10.0.0.0/24"
 
-  availability_zone = local.availability_zones[count.index]
-
-  tags = local.tags
+  
+  tags = {
+    Name = "maxtsymbaliuk-public-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
