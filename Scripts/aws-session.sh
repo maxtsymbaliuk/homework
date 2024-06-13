45d13f6e1e3f63217dcf74f87d6a13cd2eb72d0e
vagrant <vagrant@ubuntu>
2024-06-13 06:44:57 +0000

    add change in aws script

--- Scripts/aws-session.sh
+++ Scripts/aws-session.sh
@@ -2,33 +2,33 @@
 
 # Перевіряємо, чи встановлені змінні середовища для сесії
 if [[ -n "$AWS_ACCESS_KEY_ID" && -n "$AWS_SECRET_ACCESS_KEY" && -n "$AWS_SESSION_TOKEN" ]]; then
-    echo "Виявлено активну сесію AWS CLI."
-    echo "1 - завершити цю сесію та розпочати нову"
-    echo "2 - завершити цю сесію"
-    read -p "Введіть ваш вибір: " RESPONSE
+    echo "An active AWS CLI session has been detected."
+    echo "1 -  end this session and start a new one"
+    echo "2 - end this session"
+    read -p "Enter your choise:: " RESPONSE
     case $RESPONSE in
         1)
             unset AWS_ACCESS_KEY_ID
             unset AWS_SECRET_ACCESS_KEY
             unset AWS_SESSION_TOKEN
-            echo "Активну сесію було завершено. Введіть MFA код з Вашого пристрою та натисніть Enter:"
+            echo "The active session is now closed. Enter the MFA code from your device and press Enter:"
             ;;
         2)
             unset AWS_ACCESS_KEY_ID
             unset AWS_SECRET_ACCESS_KEY
             unset AWS_SESSION_TOKEN
-            echo "Активну сесію було завершено."
+            echo "The active session was over."
             exit 0
             ;;
         *)
-            echo "Невідома опція: $RESPONSE"
+            echo "WHAT???: $RESPONSE"
             exit 1
             ;;
     esac
 fi
 
 # Запитуємо MFA код для створення нової сесії
-read -p "Введіть MFA код з Вашого пристрою та натисніть Enter: " MFA_CODE
+read -p "Введіть MFA код з Вашого пристрою та натисніть Enter:" MFA_CODE
 MFA_SERIAL_NUMBER="arn:aws:iam::590184137042:mfa/maxtsymbaliuk"
 OUTPUT=$(aws sts get-session-token --serial-number $MFA_SERIAL_NUMBER --token-code $MFA_CODE 2>&1)
 
@@ -42,11 +42,11 @@ if echo $OUTPUT | grep -q 'Credentials'; then
     export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
     export AWS_SESSION_TOKEN=$SESSION_TOKEN
 
-    echo "Сесія встановлена успішно!"
+    echo "Session established successfully!"
     # Додаткова перевірка встановленої сесії
     aws sts get-caller-identity
 else
-    echo "Не вдалося отримати облікові дані. Будь ласка, перевірте ваш MFA код і спробуйте знову."
-    echo "Помилка:"
+    echo "Credentials could not be retrieved. Please check your MFA code and try again."
+    echo "Error:"
     echo $OUTPUT
 fi
